0aa9beeed5869011f46cb872fe1bf26e08ab0f89
Brad Coffield <bcoffield@gmail.com>
2019-11-30 12:44:20 -0700

    grabbing correct cheatsheet, passing values around if useInProduction

--- js/ebsco-display-1.js
+++ js/ebsco-display-1.js
@@ -1,3 +1,39 @@
 const ebscoDisplay1 = (function() {
     console.log("Hello from ebscoDisplay1.")
+    let cheatsheetPage = document.querySelector(".subjectName").id
+    console.log(cheatsheetPage);
+    const cheatsheetsRef = db2.collection("Cheatsheets").doc(cheatsheetPage);
+
+
+function runMyStuff(block, data){
+    console.log(block, "block");
+    if (block === "ebsco_api_a9h"){ebscoBlockInitialize(data)}
+}
+
+function ebscoBlockInitialize(blockData){
+console.log("ebscooo", blockData);
+}
+
+    cheatsheetsRef.get().then(function(doc) {
+        if (doc.exists) {
+            // console.log("Document data:", doc.data());
+            let data = doc.data();
+            let keys = Object.keys(data)
+            // console.log("keys = ", keys);
+            keys.forEach((i) => {
+                // look in data[i] and if anywhere in that array there is "useInProduction true"
+                console.log(i, data[i]);
+                if (data[i].some(e => e.useInProduction === true)) {
+                //    console.log("YAY",data[i]);
+                   runMyStuff(i, data[i])
+                  }
+            })
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    });
+ 
 })();
