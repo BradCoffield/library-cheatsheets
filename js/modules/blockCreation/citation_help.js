7d56b7fe830b9e9f0f4eaceaa8c349f1acb04141
Brad Coffield <bcoffield@gmail.com>
2019-12-14 10:32:17 -0700

    if OWL links, display them first

--- js/modules/blockCreation/citation_help.js
+++ js/modules/blockCreation/citation_help.js
@@ -12,13 +12,33 @@ module.exports = rawSheetData => {
         "CitationStylesRepository",
         styleWanted
       );
-      let contentForDom = `<h3>${citationStyleData.styleDisplayName}</h3><p class="heading-description">${citationStyleData.descriptionOfStyle}</p><p><h4>Available in the library</h4><ul><li><img src="${citationStyleData.styleBook.imgURL}" alt="Book cover of MLA Handbook"></img>${citationStyleData.styleBook.bookDescription} It is available for use <a href="${citationStyleData.styleBook.primoURL}" target="_blank">in the library.</a></li></ul></p><p><h4>Helpful Links</h4><ul><li>hi<li></ul></p>`;
+      console.log(citationStyleData.styleWeblinks);
+      let contentForDom = `<h3>${citationStyleData.styleDisplayName}</h3><p class="heading-description">${citationStyleData.descriptionOfStyle}</p><p><h4>Available in the library</h4><ul><li><img src="${citationStyleData.styleBook.imgURL}" alt="Book cover of MLA Handbook"></img>${citationStyleData.styleBook.bookDescription} It is available for use <a href="${citationStyleData.styleBook.primoURL}" target="_blank">in the library.</a></li></ul></p><p><h4>Helpful Links</h4><ul id="${citationStyleData.styleDisplayName}-helpful-links-ul"><li>hi<li></ul></p>`;
 
-      let domStuff = new BlockContent(contentForDom, "citation_styles-interior");
+      let domStuff = new BlockContent(
+        contentForDom,
+        "citation_styles-interior"
+      );
       domStuff.getToAppending();
+
+      let helpfulLinksParseAppend = (function() {
+        let styleLinks = citationStyleData.styleWeblinks;
+        if (styleLinks.purdueOwlLinks) {
+          let forDom = `
+          <h5>Purdue OWL</h5>
+          <li><a href="${styleLinks.purdueOwlLinks.primaryLink}" target="_blank">MLA Guide @ the OWL</a></li>
+          <li><a href="${styleLinks.purdueOwlLinks.citingBookSourcesLink}" target="_blank">Citing Book Sources</a></li>
+          <li><a href="${styleLinks.purdueOwlLinks.citingOnlineSourcesLink}" target="_blank">Citing Online Sources</a></li>
+          <li><a href="${styleLinks.purdueOwlLinks.samplePaperLink}" target="_blank">Sample Paper</a></li>`;
+          let domStuff2 = new BlockContent(
+            forDom,
+            `${citationStyleData.styleDisplayName}-helpful-links-ul`
+          );
+          domStuff2.getToAppending();
+        }
+      })();
     });
-    
-  
+
     // sheetCitationStylesArr[0].stylesWanted.forEach(styleWanted => {
     //   citationStyleData.then(function(doc) {
     //     if (doc.exists) {
