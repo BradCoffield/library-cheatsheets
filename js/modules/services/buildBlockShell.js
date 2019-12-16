85287eebf9358c05a8bea3c6d83abb4b0c3e3a3c
Brad Coffield <bcoffield@gmail.com>
2019-12-16 13:39:23 -0700

    now displaying! with description

--- js/modules/services/buildBlockShell.js
+++ js/modules/services/buildBlockShell.js
@@ -1,5 +1,5 @@
 module.exports = (blockDisplayName, blockFirestoreName) => {
-console.log("TCL: blockDisplayName", blockDisplayName)
+// console.log("TCL: blockDisplayName", blockDisplayName)
   
   const blockShell = `<div class="cheatsheet-block"><h3>${blockDisplayName}</h3><div id="${blockDisplayName}-interior"></div></div>`;
   const domElement = document.getElementById("cheatsheetsBlockWrapper");
