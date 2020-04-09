569f3cf34c847c60df3a771f7eb5fb26390f2ad2
Brad <bcoffield@gmail.com>
2020-04-09 16:21:09 -0600

    masonry meh

--- js/cheatsheets.js
+++ js/cheatsheets.js
@@ -31,55 +31,66 @@ const createDbBySubject = require("./modules/blockCreation/core/databases-by-sub
   const defaultOrderForBlocks = await getDefaultOrder();
   const dataForThisCheatsheet = await getSingleCheatsheet();
   const blocksForProduction = blocksForCheatsheet(dataForThisCheatsheet);
- 
- 
 
   //   going in the desired order if it exists as a block wanted on this page it's shell gets appended to the page
-  defaultOrderForBlocks.forEach(block => {
+  defaultOrderForBlocks.forEach((block) => {
     if (blocksForProduction.includes(block)) {
       buildBlockShell(block);
     }
   });
 
-  blocksForProduction.forEach(blockName => {
+  blocksForProduction.forEach((blockName) => {
     if (blockName === "ebsco_api_a9h") {
-      document.getElementById("ebsco_api_a9h-heading").innerHTML = "Articles from Academic Search Complete"
+      document.getElementById("ebsco_api_a9h-heading").innerHTML =
+        "Articles from Academic Search Complete";
       createEbscoApiBlock(proxyPrepend, dataForThisCheatsheet.ebsco_api_a9h);
       // ebscoBlockInitialize(dataForThisCheatsheet.ebsco_api_a9h);
     }
     if (blockName === "weblinks_block") {
-      document.getElementById("weblinks_block-heading").innerHTML = "Trusted Websites"
+      document.getElementById("weblinks_block-heading").innerHTML =
+        "Trusted Websites";
       createWeblinksBlock(); //don't need args for this one
     }
     if (blockName === "citation_styles") {
-      document.getElementById("citation_styles-heading").innerHTML = "Citing Resources"
+      document.getElementById("citation_styles-heading").innerHTML =
+        "Citing Resources";
       // console.log("citation_stylesINIT");
       createCitationBlock(dataForThisCheatsheet);
     }
     if (blockName === "primo_article_searches") {
-      document.getElementById("primo_article_searches-heading").innerHTML = "Articles from RMC Power Search"
+      document.getElementById("primo_article_searches-heading").innerHTML =
+        "Articles from RMC Power Search";
       createPrimoArticlesBlock(dataForThisCheatsheet.primo_article_searches);
     }
     if (blockName === "primo_book_searches") {
-      document.getElementById("primo_book_searches-heading").innerHTML = "Select Books from RMC Library"
+      document.getElementById("primo_book_searches-heading").innerHTML =
+        "Select Books from RMC Library";
       // console.log("primo_book_searchesINIT");
       createPrimoBooksBlock(dataForThisCheatsheet.primo_book_searches);
     }
     if (blockName === "dpla") {
-      document.getElementById("dpla-heading").innerHTML = "From the Digital Public Library of America"
+      document.getElementById("dpla-heading").innerHTML =
+        "From the Digital Public Library of America";
       createDPLABlock(dataForThisCheatsheet.dpla);
     }
     if (blockName === "primo_quick_search") {
-      document.getElementById("primo_quick_search-heading").innerHTML = "RMC Power Search: Books and more"
+      document.getElementById("primo_quick_search-heading").innerHTML =
+        "RMC Power Search: Books and more";
       createPrimoQuickSearch();
     }
-    if (blockName === "databases"){  
+    if (blockName === "databases") {
       const cheatsheetPage = document.querySelector(".subjectName").id;
-      document.getElementById("databases-heading").innerHTML = `${cheatsheetPage} Databases`
-      createDbBySubject(proxyPrepend)}
+      document.getElementById(
+        "databases-heading"
+      ).innerHTML = `${cheatsheetPage} Databases`;
+      createDbBySubject(proxyPrepend);
+    }
   });
-  
 
-  
-  
+  // init with selector
+  var msnry = new Masonry(".grid", {
+    // options...
+    itemSelector: ".grid-item",
+    columnWidth: 200,
+  });
 })();
--- js/modules/services/buildBlockShell.js
+++ js/modules/services/buildBlockShell.js
@@ -1,6 +1,5 @@
-module.exports = (blockDisplayName, blockFirestoreName) => {
-  
-  const blockShell = `<div class="cheatsheet-block" id="${blockDisplayName}-wrap"><h3 id="${blockDisplayName}-heading">${blockDisplayName}</h3><div id="${blockDisplayName}-interior" class="cheatsheet-block-interior"></div></div>`;
+module.exports = (blockDisplayName, gridValue) => {
+  const blockShell = `<div class="cheatsheet-block grid-item" id="${blockDisplayName}-wrap"><h3 id="${blockDisplayName}-heading">${blockDisplayName}</h3><div id="${blockDisplayName}-interior" class="cheatsheet-block-interior  "></div></div>`;
   const domElement = document.getElementById("cheatsheetsBlockWrapper");
   domElement.insertAdjacentHTML("beforeend", blockShell);
 };
