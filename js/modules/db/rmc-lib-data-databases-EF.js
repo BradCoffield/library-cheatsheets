17a37be854e115cf4ea93a88c20fc88291995472
Brad <bcoffield@gmail.com>
2020-04-22 13:09:26 -0600

    fixed the phil/rlst conditional

--- js/modules/db/rmc-lib-data-databases-EF.js
+++ js/modules/db/rmc-lib-data-databases-EF.js
@@ -1,7 +1,10 @@
 module.exports = (whatWeWant) => {
-  if (whatWeWant == "Philosophy" || "Religious Studies") {
+  if (whatWeWant == "Philosophy") {
     whatWeWant = "Philosophy & Religious Studies";
   }
+  if (whatWeWant == "Religious Studies") {
+    whatWeWant = "Philosophy & Religious Studies";
+  } else whatWeWant = whatWeWant;
   let allTheDocuments = [];
   return db
     .collection("databases")
